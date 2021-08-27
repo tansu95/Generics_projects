2412264a0af4b2f4ca4db26a7a5c0f08f5edd236
Your Name <you@example.com>
2021-08-27 22:44:14 +0600

    practice

---
+++ src/generics_task/Box.java
@@ -0,0 +1,25 @@
+package generics_task;
+
+public class Box<T> {
+    private T obj;
+
+    public Box(T obj) {
+        this.obj = obj;
+    }
+
+    public T getObj() {
+        return obj;
+    }
+
+    public void setObj(T obj) {
+        this.obj = obj;
+    }
+
+    @Override
+    public String toString() {
+        return "Box{" +
+                "obj = " + obj +
+                "; objType = " + obj.getClass().getName()+
+                '}';
+    }
+}
