3a8842375233a25d326d5d2727116875d717d5b3
Your Name <you@example.com>
2021-08-27 22:48:32 +0600

    practice

---
+++ src/generics_task2/Test.java
@@ -0,0 +1,12 @@
+package generics_task2;
+
+public class Test {
+    public static void main(String[] args) {
+        // параметризируем класс типом String для ключа и значения
+        Box<String, String> sample1 = new Box<>("имя", "Runtime");
+        System.out.println(sample1);
+        // параметризируем класс типом Integer для ключа и Boolean для значения
+        Box<Integer, Boolean> sample2 = new Box<>(1, Boolean.TRUE);
+        System.out.println(sample2);
+    }
+}
